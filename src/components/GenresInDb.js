aa0cb2818f51481e756ee99e44c0d06bc56c6fb8
knarf-ground <magicalblueturtle@gmail.com>
2023-11-17 16:05:57 -0300

    category

--- src/components/GenresInDb.js
+++ src/components/GenresInDb.js
@@ -6,7 +6,7 @@ function GenresInDb() {
       <div className="card shadow mb-4">
         <div className="card-header py-3">
           <h5 className="m-0 font-weight-bold text-gray-800">
-            Genres in Data Base
+            Category in Data Base
           </h5>
         </div>
         <div className="card-body">
@@ -14,26 +14,6 @@ function GenresInDb() {
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
                 <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
               </div>
             </div>
             <div className="col-lg-6 mb-4">
@@ -49,16 +29,6 @@ function GenresInDb() {
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
                 <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
               </div>
             </div>
           </div>
